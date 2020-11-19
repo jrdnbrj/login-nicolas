f786a4e0ccdf783201e875a04d7315e697590688
Jordan <www.jrdnbrj@hotmail.com>
2020-11-19 13:03:57 -0500

    Club Model

--- app/admin.py
+++ app/admin.py
@@ -3,3 +3,4 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Jugador)
+admin.site.register(Club)
--- app/models.py
+++ app/models.py
@@ -10,3 +10,11 @@ class Jugador(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Club(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    jugadores = models.ManyToManyField(Jugador, blank=True, related_name='jugadores')
+
+    def __str__(self):
+        return self.nombre
